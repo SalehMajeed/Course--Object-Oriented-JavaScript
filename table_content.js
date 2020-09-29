19570b9a6e1829d9df45e43bae767f43ba234550
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-29 11:48:05 -0400

    window in js

---
+++ table_content.js
@@ -0,0 +1,9 @@
+/*
+    1. Object Literals.
+    2. Classes.
+    3. Constructor.
+    4. Methods.
+    5. Method Chaining.
+    6. Prototype.
+    7. Prototype Inheritance.
+*/
---
+++ window.js
@@ -0,0 +1,4 @@
+var x = window
+let height = x.innerHeight;
+let width = x.innerWidth;
+console.log(width,height)
